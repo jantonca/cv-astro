46bf73f36e0e73781e8e7a9d0ddfc54b6a0e2202
Josemi Anton <development@bobeliadesign.com>
2025-07-20 14:17:31 +1000

    fix: Update site and base configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config'
-
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  site: 'https://jantonca.github.io',
+  base: '/cv-astro',
   vite: {
     plugins: [tailwindcss()],
   },
